87f5ef65af41fa8a74587bb149ebfc1afc8365ca
lorenzogrimaldi <lorenzogrimaldi@outlook.com>
2018-04-08 13:32:43 -0600

    La ira te da un gran poder, pero si no la controlas puede destruirte

--- src/components/dashboard/Index.vue
+++ src/components/dashboard/Index.vue
@@ -22,7 +22,7 @@
                 <div class="small-box bg-blue">
                   <div class="inner">
                     <h3>{{ this.atletas.length}}</h3>
-                    <p>Atletas registrados</p> 
+                     <p>Atletas registrados</p> 
                   </div>  
                   <div class="icon">
                     <i class="fa fa-users"></i>
@@ -102,7 +102,18 @@
                     <h3 class="box-title "><b>Interactuando con el Sistema</b></h3>
                   </div>
                   <div class="box-body text-justify">
-                  
+          <div class="col-md-12">
+                      Usando el menú lateral, podrá acceder a todas las opciones que posee el sistema.
+                      <br><br>
+                      Usted podrá agregar <b> ATLETAS, JUECES, MIEMBROS DE JUNTA O ENTRENADORES</b> de forma fácil y rápida.
+                      <br><br>
+                      También conseguirá llevar un registro de las <b>PLAYAS</b> donde se practica el surf y donde se realizan las competencias.
+                      <br><br>
+                      Mediante el sistema se podrán crear <b>NOTICIAS</b> las cuales serán accedidas desde el sitio web o de desde la aplicación móvil.
+                      <br><br>
+                      El sistema permitirá la creación de <b>USUARIOS</b>, los cuales podrán acceder al sistema, y poder realizar modificaciones según los permisos que se tengan concedidos. 
+                    </div>
+
                   </div>
                 </div>
               </div>
@@ -114,7 +125,16 @@
                     <h3 class="box-title "><b>Automatizacion de Procesos</b></h3>
                   </div>
                   <div class="box-body text-justify">
-                   
+                <div class="col-md-12">
+                      El sistema permitirá la creación de <b>TORNEOS (FECHAS)</b>, asignado a la playa donde se realizará el evento.
+                      <br><br>
+                      Se podrá definir las <b>CATEGORÍAS</b> participantes, así como los atletas por cada categoría.
+                      <br><br>
+                      El sistema será el responsable de la asignación de atletas para cada uno de los heats que se realicen, así como la sumatoria de puntos al final de cada heat.
+                      <br><br>
+                      Al final de cada <b>TORNEOS (FECHAS)</b> que se realice el sistema generará la tabla de puntuaciones finales de cada una de las <b>CATEGORÍAS</b>
+                    </div>
+
                   </div>
                 </div>
               </div>
