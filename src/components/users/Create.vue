85fdaf38db2589b744756df1b4d5fa0d0421d973
Alexander Guevara <alexguevara078@gmail.com>
2018-04-15 00:08:30 -0600

    Mejorando Interfaz Usuarios y Noticias

--- src/components/users/Create.vue
+++ src/components/users/Create.vue
@@ -14,17 +14,17 @@
                 <div class="col-xs-12">
                     <div class="wrapper-alert">
                         <alert type="danger" :closable="true" v-if="showAlert" @close="showAlert=false">
-                            <h4><i class="icon fa fa-ban"></i> Alert!</h4>
+                            <h4><i class="icon fa fa-ban"></i> Error!</h4>
                             <p>{{ errMsg }}</p>
                         </alert>
                         <alert type="success" :closable="true" v-if="showSuccess" @close="showSuccess=false">
-                            <h4><i class="icon fa fa-check"></i> Success!</h4>
+                            <h4><i class="icon fa fa-check"></i> Exito!</h4>
                             <p>{{ successMsg }}</p>
                         </alert>
                     </div>
                     <div class="box">
                         <div class="box-header">
-                            <h3 class="box-title">Add Usuarios</h3>
+                            <h3 class="box-title">Agregar Usuarios</h3>
                         </div>
                         
                         <div class="box-body">
@@ -45,7 +45,7 @@
 
                                          <div class="col-xs-12 col-sm-6">
                                         <div class="fgroup"  :class="{ 'has-error': errors.has('rol_edit') }">
-                                            <label for="">Position</label>
+                                            <label for="">Rol</label>
                                             <v-select
                                                 :debounce="250"
                                                 :options="roles"
@@ -106,7 +106,7 @@
                                 </div>
                                 <div class="box-footer">
                                     <div class="col-xs-12 text-right">
-                                        <button type="submit" class="btn btn-flat btn-sm btn-primary">ADD NEW</button>
+                                        <button type="submit" class="btn btn-flat btn-sm btn-primary">Agregar Nuevo</button>
                                     </div>
                                 </div>
 
@@ -188,7 +188,7 @@ if(this.createUser.rolId.descripcion=='Juez'){
                     }
                     else{
                         this.showAlert = true
-                        this.errMsg = "Form error"
+                        this.errMsg = "Error revisa el formulario"
                     }
                 });
             }
